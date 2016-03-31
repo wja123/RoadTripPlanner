7c61ebf9680ac9992ec19610f482f659e8ea3522
wja123 <w.j.alag@gmail.com>
2016-03-30 20:15:37 -0700

    commit before pull

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models/user");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/map', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
